2f40a0eb272cf6ee3322dd7b99ae902eefe6fe33
ZZZank <3410764033@qq.com>
2024-12-10 09:01:33 +0800

    cache hashcode result of EnumProperty

---
+++ src/main/java/zank/mods/datastructium/mixin/data_struct/MixinEnumProperty.java
@@ -0,0 +1,30 @@
+package zank.mods.datastructium.mixin.data_struct;
+
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * @author ZZZank
+ */
+@Mixin(EnumProperty.class)
+public class MixinEnumProperty {
+
+    @Unique
+    private Integer dataStruct$cachedHash = null;
+
+    @Inject(method = "generateHashCode", at = @At("HEAD"), cancellable = true)
+    public void replaceHash(CallbackInfoReturnable<Integer> cir) {
+        if (dataStruct$cachedHash != null) {
+            cir.setReturnValue(dataStruct$cachedHash);
+        }
+    }
+
+    @Inject(method = "generateHashCode", at = @At("RETURN"))
+    public void cacheHash(CallbackInfoReturnable<Integer> cir) {
+        dataStruct$cachedHash = cir.getReturnValue();
+    }
+}
