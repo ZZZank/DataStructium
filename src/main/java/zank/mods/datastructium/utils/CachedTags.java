3b08905e0b647dce69daa4acb9dbfb174e19c577
ZZZank <3410764033@qq.com>
2025-02-06 23:14:41 +0800

    log it

--- src/main/java/zank/mods/datastructium/utils/CachedTags.java
+++ src/main/java/zank/mods/datastructium/utils/CachedTags.java
@@ -3,6 +3,7 @@ package zank.mods.datastructium.utils;
 import lombok.val;
 import net.minecraft.nbt.*;
 import zank.mods.datastructium.DSConfig;
+import zank.mods.datastructium.DataStructium;
 
 import java.util.Arrays;
 
@@ -11,8 +12,8 @@ import java.util.Arrays;
  */
 public class CachedTags {
 
-    public static final int START = DSConfig.NUMBER_TAG_CACHE_START;
-    public static final int END = DSConfig.NUMBER_TAG_CACHE_END;
+    public static final int START = DSConfig.ENABLE_NUMBER_TAG_CACHE ? DSConfig.NUMBER_TAG_CACHE_START : 0;
+    public static final int END = DSConfig.ENABLE_NUMBER_TAG_CACHE ? DSConfig.NUMBER_TAG_CACHE_END : 0;
     public static final int SIZE = END - START;
 
     private static final FloatTag[] TAG_FLOATS = new FloatTag[SIZE];
@@ -31,6 +32,12 @@ public class CachedTags {
         Arrays.setAll(TAG_DOUBLES, i -> new DoubleTag(i + START));
         Arrays.setAll(TAG_INTS, i -> new IntTag(i + START));
         val end = System.currentTimeMillis();
+        DataStructium.LOGGER.info(
+            "Number Tag Cache for int/double/float in range [{}, {}) initialized, took {}ms",
+            START,
+            END,
+            end - start
+        );
     }
 
     public static IntTag getCachedInt(int i) {
