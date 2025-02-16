8717ef5e6b78f6391d38f0b93efe21f306548301
ZZZank <3410764033@qq.com>
2025-02-17 01:08:21 +0800

    patch JER mob egg lookup, and fix a bug

---
+++ src/main/java/zank/mods/datastructium/mixin/mods/jer/MixinMobWrapper.java
@@ -0,0 +1,42 @@
+package zank.mods.datastructium.mixin.mods.jer;
+
+import com.google.common.base.Suppliers;
+import jeresources.entry.MobEntry;
+import jeresources.jei.mob.MobWrapper;
+import lombok.val;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.common.ForgeSpawnEggItem;
+import org.spongepowered.asm.mixin.*;
+
+import java.util.function.Supplier;
+
+/**
+ * @author ZZZank
+ */
+@Mixin(MobWrapper.class)
+@Pseudo
+public class MixinMobWrapper {
+    @Shadow
+    @Final
+    private MobEntry mob;
+
+    @Unique
+    private static final Supplier<ItemStack> dataStruct$fallbackItem = Suppliers.memoize(() ->
+        Items.BARRIER.getDefaultInstance().copy().setHoverName(new TextComponent("No Egg"))
+    );
+
+    /**
+     * @author ZZZank
+     * @reason use forge's map based egg lookup, add fallback if no spawn egg found
+     */
+    @Overwrite
+    public ItemStack getSpawnEgg() {
+        val item = ForgeSpawnEggItem.fromEntityType(this.mob.getEntity().getType());
+        if (item == null) {
+            return dataStruct$fallbackItem.get();
+        }
+        return item.getDefaultInstance();
+    }
+}
