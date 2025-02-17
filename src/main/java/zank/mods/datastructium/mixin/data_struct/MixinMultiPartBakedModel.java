eee876d87c2bf86b6a51b6a7b5c6c408a32ac819
ZZZank <3410764033@qq.com>
2025-02-18 00:15:22 +0800

    simplify MixinMultiPartBakedModel

--- src/main/java/zank/mods/datastructium/mixin/data_struct/MixinMultiPartBakedModel.java
+++ src/main/java/zank/mods/datastructium/mixin/data_struct/MixinMultiPartBakedModel.java
@@ -1,22 +1,15 @@
 package zank.mods.datastructium.mixin.data_struct;
 
 import com.mojang.datafixers.util.Pair;
-import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.client.resources.model.MultiPartBakedModel;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import zank.mods.datastructium.utils.CollectUtils;
 
-import java.util.BitSet;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Predicate;
 
 /**
@@ -25,19 +18,8 @@ import java.util.function.Predicate;
 @Mixin(value = MultiPartBakedModel.class, priority = 345)
 public abstract class MixinMultiPartBakedModel {
 
-    @Mutable
-    @Shadow
-    @Final
-    private Map<BlockState, BitSet> selectorCache;
-
-    @Mutable
-    @Shadow
-    @Final
-    private List<Pair<Predicate<BlockState>, BakedModel>> selectors;
-
-    @Inject(method = "<init>", at = @At("RETURN"))
-    public void replaceInternal(List<Pair<Predicate<BlockState>, BakedModel>> list, CallbackInfo ci) {
-        this.selectorCache = new Reference2ObjectOpenHashMap<>(this.selectorCache);
-        this.selectors = CollectUtils.reduceSmallList(this.selectors);
+    @ModifyVariable(method = "<init>", at = @At("HEAD"), ordinal = 0)
+    private List<Pair<Predicate<BlockState>, BakedModel>> shrinkList(List<Pair<Predicate<BlockState>, BakedModel>> list) {
+        return CollectUtils.reduceSmallList(list);
     }
 }
