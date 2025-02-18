d2323f2bd9a9a682a45a718a8cc1add04b8f3594
ZZZank <3410764033@qq.com>
2025-02-18 16:49:33 +0800

    fix JER correctly

--- src/main/java/zank/mods/datastructium/mixin/mods/jeresources/MixinMobWrapper.java
+++ src/main/java/zank/mods/datastructium/mixin/mods/jeresources/MixinMobWrapper.java
@@ -3,6 +3,8 @@ package zank.mods.datastructium.mixin.mods.jeresources;
 import jeresources.entry.MobEntry;
 import jeresources.jei.mob.MobWrapper;
 import lombok.val;
+import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.ingredients.IIngredients;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.ForgeSpawnEggItem;
 import org.spongepowered.asm.mixin.*;
@@ -19,23 +21,15 @@ public class MixinMobWrapper {
 
     /**
      * @author ZZZank
-     * @reason use forge's map based egg lookup, return empty stack if no spawn egg found
+     * @reason use forge's map based egg lookup, return null stack if no spawn egg found
+     * @see jeresources.jei.mob.MobCategory#setRecipe(IRecipeLayout, MobWrapper, IIngredients)
      */
     @Overwrite
     public ItemStack getSpawnEgg() {
         val item = ForgeSpawnEggItem.fromEntityType(this.mob.getEntity().getType());
         if (item == null) {
-            return ItemStack.EMPTY;
+            return null;
         }
         return item.getDefaultInstance();
-    }
-
-    /**
-     * @author ZZZank
-     * @reason the {@link #getSpawnEgg()} method will never return null, even in original logic
-     */
-    @Overwrite
-    public boolean hasSpawnEgg() {
-        return !this.getSpawnEgg().isEmpty();
     }
 }
