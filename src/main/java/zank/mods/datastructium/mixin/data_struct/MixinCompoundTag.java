ed51c6c531f8a02cf664dae4ba577a4f787e28bd
ZZZank <3410764033@qq.com>
2025-02-28 00:03:29 +0800

    replace compound tag internal more aggressively

--- src/main/java/zank/mods/datastructium/mixin/data_struct/MixinCompoundTag.java
+++ src/main/java/zank/mods/datastructium/mixin/data_struct/MixinCompoundTag.java
@@ -4,7 +4,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import zank.mods.datastructium.DSConfig;
 import zank.mods.datastructium.utils.TieredInternalMap;
 
@@ -16,10 +16,10 @@ import java.util.Map;
 @Mixin(value = CompoundTag.class, priority = 456)
 public abstract class MixinCompoundTag {
 
-    @ModifyArg(method = "<init>()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;<init>(Ljava/util/Map;)V"))
+    @ModifyVariable(method = "<init>(Ljava/util/Map;)V", at = @At(value = "HEAD"), ordinal = 0, argsOnly = true)
     private static Map<String, Tag> replaceInternal(Map<String, Tag> map) {
         if (DSConfig.TIERED_COMPOUND_TAG_INTERNAL) {
-            return new TieredInternalMap<>();
+            return new TieredInternalMap<>(map);
         }
         return map;
     }
