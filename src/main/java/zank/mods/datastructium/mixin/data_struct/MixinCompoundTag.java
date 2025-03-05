81886759614018cdb890779eb998dd7b68b9aa70
ZZZank <3410764033@qq.com>
2025-03-05 11:15:07 +0800

    fix mixin target

--- src/main/java/zank/mods/datastructium/mixin/data_struct/MixinCompoundTag.java
+++ src/main/java/zank/mods/datastructium/mixin/data_struct/MixinCompoundTag.java
@@ -1,18 +1,12 @@
 package zank.mods.datastructium.mixin.data_struct;
 
-import lombok.val;
 import net.minecraft.nbt.*;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import zank.mods.datastructium.DSConfig;
 import zank.mods.datastructium.utils.TieredInternalMap;
 
-import java.io.DataInput;
-import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -23,16 +17,19 @@ public abstract class MixinCompoundTag {
 
     @ModifyVariable(method = "<init>(Ljava/util/Map;)V", at = @At(value = "HEAD"), ordinal = 0, argsOnly = true)
     private static Map<String, Tag> replaceInternal(Map<String, Tag> map) {
-        if (DSConfig.TIERED_COMPOUND_TAG_INTERNAL && !(map instanceof TieredInternalMap)) {
+        if (map instanceof TieredInternalMap) {
+            return map;
+        }
+        if (DSConfig.TIERED_COMPOUND_TAG_INTERNAL) {
             return new TieredInternalMap<>(map);
         }
         return map;
     }
 
-    @Mixin(targets = "net.minecraft.nbt.CompoundTag$1")
+    @Mixin(targets = "net.minecraft.nbt.CompoundNBT$1", remap = false)
     public static abstract class MixinTagType {
 
-        @ModifyVariable(method = "load*", at = @At("STORE"))
+        @ModifyVariable(method = "func_225649_b_*", at = @At("STORE"))
         private Map<String, Tag> replaceMap(Map<String, Tag> map) {
             if (DSConfig.TIERED_COMPOUND_TAG_INTERNAL) {
                 return new TieredInternalMap<>();
