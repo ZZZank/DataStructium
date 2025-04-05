5f404197f6107ef094b343c22ae377197a5d32c9
ZZZank <3410764033@qq.com>
2025-04-06 01:12:42 +0800

    add javadoc and mixin annotation

--- src/main/java/zank/mods/datastructium/mixin/vec3i_hashing/MixinVec3i.java
+++ src/main/java/zank/mods/datastructium/mixin/vec3i_hashing/MixinVec3i.java
@@ -3,6 +3,7 @@ package zank.mods.datastructium.mixin.vec3i_hashing;
 import it.unimi.dsi.fastutil.HashCommon;
 import net.minecraft.core.Vec3i;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 /**
@@ -24,7 +25,12 @@ public abstract class MixinVec3i {
     @Shadow
     private int z;
 
+    /**
+     * @author ZZZank
+     * @reason an algorithm with less hash collision in general
+     */
     @Override
+    @Overwrite
     public int hashCode() {
         return HashCommon.mix(HashCommon.mix(this.x) + this.y) + this.z;
     }
