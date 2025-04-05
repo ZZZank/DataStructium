b1b0fdacbcea0579a384d2661abe5e8687c0a11d
ZZZank <3410764033@qq.com>
2025-04-05 17:30:50 +0800

    add mixin plugin to prepare for config overrides

---
+++ src/main/java/zank/mods/datastructium/mixin/DataStructiumMixinPlugin.java
@@ -0,0 +1,93 @@
+package zank.mods.datastructium.mixin;
+
+import lombok.val;
+import net.minecraftforge.fml.loading.FMLLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Supplier;
+
+/**
+ * @author ZZZank
+ */
+public class DataStructiumMixinPlugin implements IMixinConfigPlugin {
+
+    public static final String MIXIN_PACKAGE_ROOT = DataStructiumMixinPlugin.class.getPackage().getName() + '.';
+    public static final Logger LOGGER = LogManager.getLogger(DataStructiumMixinPlugin.class.getSimpleName());
+    public static final Map<String, Supplier<Boolean>> OVERRIDES = new HashMap<>();
+
+    static {
+        OVERRIDES.put("mods.kubejs", modPresent("kubejs"));
+        OVERRIDES.put("mods.masterfulmachinery", modPresent("masterfulmachinery"));
+        OVERRIDES.put("mods.jeresources", modPresent("jeresources"));
+        OVERRIDES.put("mods.industrialforegoing", modPresent("industrialforegoing"));
+        OVERRIDES.put("mods.farmersdelight", modPresent("farmersdelight"));
+    }
+
+    private static Supplier<Boolean> modPresent(String modId) {
+        return () -> FMLLoader.getLoadingModList().getModFileById(modId) != null;
+    }
+
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        val dotMixinClass = mixinClassName.replace('/', '.');
+        if (!dotMixinClass.startsWith(MIXIN_PACKAGE_ROOT)) {
+            LOGGER.error(
+                "Expected mixin '{}' to start with package root '{}', treating as foreign and disabling!",
+                dotMixinClass,
+                MIXIN_PACKAGE_ROOT
+            );
+            return false;
+        }
+
+        val trimmed = dotMixinClass.substring(MIXIN_PACKAGE_ROOT.length());
+
+        val lastIndex = trimmed.lastIndexOf('.');
+        if (lastIndex == -1) {
+            // not categorized
+            return true;
+        }
+
+        val key = trimmed.substring(0, lastIndex);
+        val override = OVERRIDES.get(key);
+        if (override == null) {
+            // no override
+            return true;
+        }
+
+        return override.get();
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
