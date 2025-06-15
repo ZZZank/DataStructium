cdaeeb8b97e4f8739127cb57617c29c0e94e4f56
ZZZank <3410764033@qq.com>
2025-06-15 10:22:36 +0800

    append class scanning option in a cleaner way

--- src/main/java/zank/mods/datastructium/mixin/misc/MixinScanner.java
+++ src/main/java/zank/mods/datastructium/mixin/misc/MixinScanner.java
@@ -2,10 +2,9 @@ package zank.mods.datastructium.mixin.misc;
 
 import net.minecraftforge.fml.loading.moddiscovery.Scanner;
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 /**
  * @author ZZZank
@@ -13,8 +12,12 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(value = Scanner.class, remap = false)
 public class MixinScanner {
 
-    @Redirect(method = "fileVisitor", at = @At(value = "INVOKE", target = "Lorg/objectweb/asm/ClassReader;accept(Lorg/objectweb/asm/ClassVisitor;I)V"))
-    private void addScanFlags(ClassReader instance, ClassVisitor classVisitor, int parsingOptions) {
-        instance.accept(classVisitor, parsingOptions | ClassReader.SKIP_CODE | ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES);
+    @ModifyArg(
+        method = "fileVisitor",
+        at = @At(value = "INVOKE", target = "Lorg/objectweb/asm/ClassReader;accept(Lorg/objectweb/asm/ClassVisitor;I)V"),
+        index = 2
+    )
+    private static int appendScanFlags(int parsingOptions) {
+        return parsingOptions | ClassReader.SKIP_CODE | ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES;
     }
 }
