2c0122b88076df68e3aa872389877d0062e12600
ZZZank <3410764033@qq.com>
2025-06-15 09:40:53 +0800

    reload shader cache on Oculus reload

---
+++ src/main/java/zank/mods/datastructium/mixin/mods/oculus/MixinIris.java
@@ -0,0 +1,22 @@
+package zank.mods.datastructium.mixin.mods.oculus;
+
+import net.coderbot.iris.Iris;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import zank.mods.datastructium.DSConfig;
+import zank.mods.datastructium.pools.ShaderCacheLoader;
+
+/**
+ * @author ZZZank
+ */
+@Mixin(value = Iris.class, remap = false)
+public class MixinIris {
+    @Inject(method = "reload", at = @At("HEAD"))
+    private static void reloadCache(CallbackInfo ci) {
+        if (DSConfig.CACHE_SHADER_UNIFORMS) {
+            ShaderCacheLoader.reload("Oculus Shader Reload");
+        }
+    }
+}
---
+++ src/main/java/zank/mods/datastructium/mixin/mods/oculus/MixinPipelineManager.java
@@ -0,0 +1,34 @@
+package zank.mods.datastructium.mixin.mods.oculus;
+
+import net.coderbot.iris.pipeline.PipelineManager;
+import net.coderbot.iris.pipeline.WorldRenderingPipeline;
+import net.coderbot.iris.shaderpack.materialmap.NamespacedId;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import zank.mods.datastructium.DSConfig;
+import zank.mods.datastructium.pools.ShaderCacheLoader;
+
+/**
+ * @author ZZZank
+ */
+
+@Mixin(value = PipelineManager.class, remap = false)
+public abstract class MixinPipelineManager {
+    @Inject(
+        method = "preparePipeline",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/coderbot/iris/IrisLogging;info(Ljava/lang/String;[Ljava/lang/Object;)V"
+        )
+    )
+    private void reloadCacheOnCreatedPipeline(
+        NamespacedId currentDimension,
+        CallbackInfoReturnable<WorldRenderingPipeline> cir
+    ) {
+        if (DSConfig.CACHE_SHADER_UNIFORMS) {
+            ShaderCacheLoader.reload("new Oculus pipeline");
+        }
+    }
+}
