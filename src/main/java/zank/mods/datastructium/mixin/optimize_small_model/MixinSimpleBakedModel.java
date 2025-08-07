bb305aa3a0a0835fcaa667b9efad2a9fddfe8690
ZZZank <3410764033@qq.com>
2025-08-07 23:37:11 +0800

    patch SimpleBakedModel in a cleaner way

--- src/main/java/zank/mods/datastructium/mixin/optimize_small_model/MixinSimpleBakedModel.java
+++ src/main/java/zank/mods/datastructium/mixin/optimize_small_model/MixinSimpleBakedModel.java
@@ -2,18 +2,11 @@ package zank.mods.datastructium.mixin.optimize_small_model;
 
 import lombok.val;
 import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.client.renderer.block.model.ItemOverrides;
-import net.minecraft.client.renderer.block.model.ItemTransforms;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.SimpleBakedModel;
 import net.minecraft.core.Direction;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import zank.mods.datastructium.utils.CollectUtils;
 
 import java.util.List;
@@ -25,29 +18,16 @@ import java.util.Map;
 @Mixin(SimpleBakedModel.class)
 public abstract class MixinSimpleBakedModel {
 
-    @Mutable
-    @Shadow
-    @Final
-    protected List<BakedQuad> unculledFaces;
-    @Shadow
-    @Final
-    protected Map<Direction, List<BakedQuad>> culledFaces;
+    @ModifyVariable(method = "<init>", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private static List<BakedQuad> reduceUnculledFaces(List<BakedQuad> unculledFaces) {
+        return CollectUtils.reduceSmallList(unculledFaces);
+    }
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void replaceInternal(
-        List<BakedQuad> list,
-        Map<Direction, List<BakedQuad>> map,
-        boolean bl,
-        boolean bl2,
-        boolean bl3,
-        TextureAtlasSprite arg,
-        ItemTransforms arg2,
-        ItemOverrides arg3,
-        CallbackInfo ci
-    ) {
-        this.unculledFaces = CollectUtils.reduceSmallList(this.unculledFaces);
-        for (val entry : this.culledFaces.entrySet()) {
+    @ModifyVariable(method = "<init>", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private static Map<Direction, List<BakedQuad>> reduceCulledFaces(Map<Direction, List<BakedQuad>> culledFaces) {
+        for (val entry : culledFaces.entrySet()) {
             entry.setValue(CollectUtils.reduceSmallList(entry.getValue()));
         }
+        return culledFaces;
     }
 }
