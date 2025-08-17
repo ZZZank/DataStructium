28dcec6f8d612f6abcb9059a0b9eb97326286936
ZZZank <3410764033@qq.com>
2025-08-17 12:16:16 +0800

    prevent patch from affecting MM machine process speed

--- src/main/java/zank/mods/datastructium/mixin/mods/masterfulmachinery/structure_check/MixinControllerBlockEntity.java
+++ src/main/java/zank/mods/datastructium/mixin/mods/masterfulmachinery/structure_check/MixinControllerBlockEntity.java
@@ -2,10 +2,14 @@ package zank.mods.datastructium.mixin.mods.masterfulmachinery.structure_check;
 
 import com.ticticboooom.mods.mm.block.tile.ControllerBlockEntity;
 import com.ticticboooom.mods.mm.block.tile.UpdatableTile;
+import com.ticticboooom.mods.mm.data.MachineStructureRecipe;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import zank.mods.datastructium.DSConfig;
 
@@ -21,23 +25,29 @@ public abstract class MixinControllerBlockEntity extends UpdatableTile {
     @Inject(
         method = "tick",
         at = @At(
-            value = "FIELD",
-            target = "Lcom/ticticboooom/mods/mm/block/tile/ControllerBlockEntity;processData:Lcom/ticticboooom/mods/mm/model/ProcessUpdate;",
-            ordinal = 0,
-            remap = false
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/item/crafting/RecipeManager;getAllRecipesFor(Lnet/minecraft/world/item/crafting/RecipeType;)Ljava/util/List;"
         ),
         cancellable = true
     )
-    private void skipActionByInterval(CallbackInfo ci) {
+    private void skipStructureSearch(CallbackInfo ci) {
         if (dataStructium$counter != 0) {
-            ci.cancel();
             this.update(); // this will happen regardless of `foundStructure`
+            ci.cancel();
         }
 
         dataStructium$counter++;
         if (dataStructium$counter == DSConfig.MM_STRUCTURE_CHECK_INTERVAL) {
             dataStructium$counter = 0;
         }
+    }
+
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lcom/ticticboooom/mods/mm/data/MachineStructureRecipe;matchesSpecificTransform(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/Level;I)Z"))
+    private boolean skipStructureValidate(MachineStructureRecipe instance, BlockPos controllerPos, Level world, int index) {
+        if (dataStructium$counter == 0) {
+            return instance.matchesSpecificTransform(controllerPos, world, index);
+        }
+        return true;
     }
 
     private MixinControllerBlockEntity() {
